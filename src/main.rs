564b636d4131e4e7da8bc200ceb78fcb04922acf
Niklas von Minckwitz <niklas.v.minckwitz@googlemail.com>
2024-01-30 13:10:26 +0100

    pass step 3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Uncomment this block to pass the first stage
-use std::{net::TcpListener, io::Write};
-use anyhow::Result;
+use std::{net::TcpListener, io::{Write, Read}};
+use anyhow::{Result, anyhow, Context};
 
 fn main() -> Result<()>{
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -14,8 +14,25 @@ fn main() -> Result<()>{
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
-                stream.flush()?;
+
+                let mut input_buf: [u8; 128] = [0; 128];
+                stream.read(&mut input_buf)?;
+
+                let input_string = String::from_utf8(input_buf.to_vec())?;
+                let path = extract_path(&input_string);
+                match path {
+                    Some("/") => {
+                        stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+                        stream.flush()?;
+                    },
+                    Some(_) => {
+                        stream.write(b"HTTP/1.1 404 NOT FOUND\r\n\r\n")?;
+                        stream.flush()?;
+                    },
+                    None => todo!(),
+                }
+
+
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -24,4 +41,25 @@ fn main() -> Result<()>{
     }
 
     Ok(())
+}
+
+fn extract_path(input: &str) -> Option<&str> {
+    input
+        .split("\r\n")
+        .take(1)
+        .flat_map(|x| x.split(" "))
+        .nth(1)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_extract_path() -> Result<()> {
+        let input = "GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\n\r\n".to_string();
+
+        let path = extract_path(&input);
+        assert_eq!(path, Some("/index.html"));
+        Ok(())
+    }
 }
